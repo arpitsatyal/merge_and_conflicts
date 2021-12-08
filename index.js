472aa6c045901fb87cfd531a556939fbcbd5f8ee
joseff-regmi <joseffregmi111@gmail.com>
2021-12-08 19:24:50 +0545

    changed parameters of the function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function addNumbers(n1, n2, n3) {
-    return n1 + n2 + n3
+function addNumbers(n1, n2) {
+    return n1 + n2
 }
 
-const sum = addNumbers(1,2)
+const sum = addNumbers(4,2)
 console.log('the sum is: ' + sum)
